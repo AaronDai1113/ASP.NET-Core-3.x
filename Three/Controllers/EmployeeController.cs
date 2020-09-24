47a337cb411bae358c242a6f17ebc5211991e9ca
AaronDai1113 <aaron11388@outlook.com>
2020-09-24 16:52:06 +0800

    924

--- Three/Controllers/EmployeeController.cs
+++ Three/Controllers/EmployeeController.cs
@@ -27,5 +27,18 @@ namespace Three.Controllers
                 DepartmentId=departmentId
             });
         }
+        public async Task<IActionResult> Add(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _employeeService.Add(model);
+            }
+            return RedirectToAction(nameof(Index), new { departmentId = model.DepartmentId });
+        }
+        public async Task<IActionResult> Fire(int employeeId)
+        {
+            var employee = await _employeeService.Fire(employeeId);
+            return RedirectToAction(nameof(Index), new { departmentId = employee.DepartmentId });
+        }
     }
 }
